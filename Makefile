9bdb7379d37283df7f1fcd59ab5911ccb991a490
webpnk <tommy.roman.hater@gmail.com>
2025-01-03 16:43:21 +0200

    sitemap

--- Makefile
+++ Makefile
@@ -10,3 +10,6 @@ deploy-proxy:
 
 export:
 	npm run build && docker compose restart ssr && docker compose run --rm -e STATIC_MODE=true app php artisan export && npm run post-export -- http://localhost:8008 https://stalker2mods.pages.dev
+
+sitemap:
+	docker compose run -e APP_DEBUG=false -e APP_URL=https://stalker2mods.pro -e APP_ENV=production --rm app php artisan sitemap:generate
---
+++ app/Console/Commands/SitemapGenerate.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Mod;
+use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Builder;
+use Spatie\Crawler\Crawler;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\SitemapGenerator;
+use Spatie\Sitemap\Tags\Url;
+use TomatoPHP\FilamentCms\Models\Category;
+use TomatoPHP\FilamentCms\Models\Post;
+
+class SitemapGenerate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sitemap:generate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $sitemap = Sitemap::create();
+
+        $sitemap->add(route('home'));
+
+        $sitemap->add(
+            Url::create(route('mods.list'))
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
+        );
+
+        $mods = Mod::all();
+
+        foreach ($mods as $mod) {
+            $sitemap->add(
+                Url::create(route('mods.info', $mod->id))
+                    ->setChangeFrequency(Url::CHANGE_FREQUENCY_NEVER)
+                    ->setLastModificationDate($mod->updated_at)
+            );
+        }
+
+        $sitemap->add(
+            Url::create(route('blog.list'))
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
+        );
+
+        $posts = Post::query()
+            ->with('categories')
+            ->where('is_published', true);
+
+        foreach ($posts as $post) {
+            $sitemap->add(
+                Url::create(route('blog.post', $post->category->slug, $post->slug))
+                    ->setChangeFrequency(Url::CHANGE_FREQUENCY_NEVER)
+                    ->setLastModificationDate($post->updated_at)
+            );
+        }
+
+        $categories = Category::query()
+            ->where('is_active', true)
+            ->where('show_in_menu', true)
+            ->get();
+
+        foreach ($categories as $category) {
+            $sitemap->add(
+                Url::create(route('blog.list', $category->slug))
+                    ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
+            );
+        }
+
+        $sitemap->writeToFile('public/sitemap.xml');
+    }
+}
